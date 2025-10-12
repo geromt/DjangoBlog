719502ae13a3e0f14893587981f908cd4442fb6c
geromt <gerardomartinez205@gmail.com>
2025-10-11 22:37:41 -0600

    feat: add UUID primary key to Article model

--- ArticlesServer/models.py
+++ ArticlesServer/models.py
@@ -1,8 +1,11 @@
+import uuid
+
 from django.db import models
 from django_dto import DTOMixin
 
 
 class Article(models.Model, DTOMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.CharField(max_length=100)
