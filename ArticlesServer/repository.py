42916a0a72baf2970220deb380ccc1443e27107a
geromt <gerardomartinez205@gmail.com>
2025-10-13 07:59:00 -0600

    feat: implement ArticleRepository with CRUD operations for articles

--- ArticlesServer/repository.py
+++ ArticlesServer/repository.py
@@ -1,11 +1,37 @@
-import uuid
-from datetime import datetime
-import dataclasses
+from ArticlesServer.models import Article
 
 
-@dataclasses.dataclass
-class ArticleDTO:
-    id: uuid.UUID
-    title: str
-    content: str
-    created_at: datetime
+class ArticleRepository:
+    def __init__(self):
+        self.queryset = Article.objects.all()
+
+    def get_by_id(self, id):
+        return self.queryset.filter(id=id).first()
+
+    def get(self):
+        return self.queryset.all()
+
+    def create(self, **kwargs):
+        article = Article.objects.create(**kwargs)
+        return article
+
+    def update(self, id, **kwargs):
+        article = self.get_by_id(id)
+        if article:
+            for key, value in kwargs.items():
+                setattr(article, key, value)
+            article.save()
+        return article
+
+    def delete(self, id):
+        article = self.get_by_id(id)
+        if article:
+            article.delete()
+        return article
+
+    def filter(self, **kwargs):
+        return self.queryset.filter(**kwargs)
+
+    def save(self, article):
+        article.save()
+        return article
